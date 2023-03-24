c2102a0c97b49790c565a75ff371aa2b0d20bf83
david.huang <david.huang@ark88.net>
2023-03-24 11:50:59 +0800

    add device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(DevicePreview(builder: (context) => const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
