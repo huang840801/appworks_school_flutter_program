0cdde127a037dd2ee6d54460904e59963c9a076c
黃瓘閎 <david840801@gmail.com>
2023-04-08 19:26:58 +0800

    implement InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ShareDataWidget extends InheritedWidget {
+  const ShareDataWidget({
+    Key? key,
+    required this.selectedColorIndex,
+    required this.selectedSizeIndex,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final int selectedColorIndex;
+  final int selectedSizeIndex;
+
+  static ShareDataWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+  }
+
+  @override
+  bool updateShouldNotify(ShareDataWidget oldWidget) {
+    return oldWidget.selectedColorIndex != selectedColorIndex;
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -26,151 +47,156 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int selectedColorIndex = -1;
   int selectedSizeIndex = -1;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    const Text('顏色', style: TextStyle(color: Colors.black, fontSize: 16)),
-                    const SizedBox(width: 10),
-                    Container(color: const Color(0x19333333), height: 18, width: 1),
-                    const SizedBox(width: 5),
-                    SingleSelectColor(
-                      selectedColorIndex: selectedColorIndex,
-                      colorList: const [Colors.green, Colors.deepOrange],
-                      onSelect: (index) {
-                        setState(() {
-                          selectedSizeIndex = -1;
-                          selectedColorIndex = index;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    const Text('尺寸', style: TextStyle(color: Colors.black, fontSize: 16)),
-                    const SizedBox(width: 10),
-                    Container(color: const Color(0x19333333), height: 18, width: 1),
-                    const SizedBox(width: 10),
-                    SingleSelectSize(
-                      selectedSizeIndex: selectedSizeIndex,
-                      sizeList: const ['S', 'M', 'L'],
-                      onSelect: (index) {
-                        setState(() {
-                          selectedSizeIndex = index;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+    return ShareDataWidget(
+      selectedColorIndex: selectedColorIndex,
+      selectedSizeIndex: selectedSizeIndex,
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Text('顏色', style: TextStyle(color: Colors.black, fontSize: 16)),
+                      const SizedBox(width: 10),
+                      Container(color: const Color(0x19333333), height: 18, width: 1),
+                      const SizedBox(width: 5),
+                      SingleSelectColor(
+                        colorList: const [Colors.green, Colors.deepOrange],
+                        onSelect: (selectedColorIndex) {
+                          setState(() {
+                            this.selectedColorIndex = selectedColorIndex;
+                            selectedSizeIndex = -1;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      const Text('尺寸', style: TextStyle(color: Colors.black, fontSize: 16)),
+                      const SizedBox(width: 10),
+                      Container(color: const Color(0x19333333), height: 18, width: 1),
+                      const SizedBox(width: 10),
+                      SingleSelectSize(
+                        sizeList: const ['S', 'M', 'L'],
+                        onSelect: (index) {
+                          setState(() {
+                            selectedSizeIndex = index;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
 
-class SingleSelectColor extends StatefulWidget {
+class SingleSelectColor extends StatelessWidget {
   final List<MaterialColor> colorList;
   final Function(int) onSelect;
-  int selectedColorIndex;
 
-  SingleSelectColor({super.key,
-    required this.selectedColorIndex,
-    required this.colorList,
-    required this.onSelect});
-
-  @override
-  SingleSelectColorState createState() => SingleSelectColorState();
-}
-
-class SingleSelectColorState extends State<SingleSelectColor> {
+  const SingleSelectColor({super.key, required this.colorList, required this.onSelect});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: List<Widget>.generate(
-        widget.colorList.length,
-            (int index) {
+        colorList.length,
+        (int index) {
           return Row(
             children: [
               OutlinedButton(
                   onPressed: () {
-                    widget.onSelect(index);
+                    onSelect(index);
                   },
                   style: ButtonStyle(
                     tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     minimumSize: MaterialStateProperty.all(const Size(40, 40)),
                     padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(0)),
-                    side: MaterialStateProperty.all(BorderSide(color: widget.selectedColorIndex == index ? Colors.black : Colors.transparent, width: 1.0, style: BorderStyle.solid)),
+                    side: MaterialStateProperty.all(BorderSide(color: getColor(context, index), width: 1.0, style: BorderStyle.solid)),
                     shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
                   ),
-                  child: Container(color: widget.colorList[index], height: 18, width: 18)),
+                  child: Container(color: colorList[index], height: 18, width: 18)),
             ],
           );
         },
       ),
     );
   }
+
+  Color getColor(BuildContext context, int index) {
+    if (ShareDataWidget.of(context) == null) {
+      return Colors.transparent;
+    } else {
+      return ShareDataWidget.of(context)!.selectedColorIndex == index ? Colors.black : Colors.transparent;
+    }
+  }
 }
 
-class SingleSelectSize extends StatefulWidget {
+class SingleSelectSize extends StatelessWidget {
   final List<String> sizeList;
   final Function(int) onSelect;
-  int selectedSizeIndex;
+  final grey = const Color.fromARGB(255, 87, 99, 108);
+  final white = Colors.white;
 
-  SingleSelectSize({super.key,
-    required this.selectedSizeIndex,
-    required this.sizeList,
-    required this.onSelect});
-
-  @override
-  SingleSelectSizeState createState() => SingleSelectSizeState();
-}
-
-class SingleSelectSizeState extends State<SingleSelectSize> {
+  const SingleSelectSize({super.key, required this.sizeList, required this.onSelect});
 
   @override
   Widget build(BuildContext context) {
-    const grey = Color.fromARGB(255, 87, 99, 108);
-    const white = Colors.white;
+    Color getBackgroundColor(int index) {
+      if (ShareDataWidget.of(context) == null) {
+        return white;
+      } else {
+        return ShareDataWidget.of(context)!.selectedSizeIndex == index ? white : grey;
+      }
+    }
+
+    Color getTextColor(int index) {
+      if (ShareDataWidget.of(context) == null) {
+        return white;
+      } else {
+        return ShareDataWidget.of(context)!.selectedSizeIndex == index ? grey : white;
+      }
+    }
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: List<Widget>.generate(
-        widget.sizeList.length,
-            (int index) {
+        sizeList.length,
+        (int index) {
           return Row(
             children: [
               ElevatedButton(
                 onPressed: () {
-                  widget.onSelect(index);
+                  onSelect(index);
                 },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.only(top: 3, bottom: 3, left: 10, right: 10),
                   minimumSize: Size.zero,
                   tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  backgroundColor: widget.selectedSizeIndex == index ? white : grey,
+                  backgroundColor: getBackgroundColor(index),
                 ),
-                child: Text(widget.sizeList[index], style: TextStyle(color: widget.selectedSizeIndex == index ? grey : white, fontSize: 12)),
+                child: Text(sizeList[index], style: TextStyle(color: getTextColor(index), fontSize: 12)),
               ),
               const SizedBox(width: 10),
             ],
