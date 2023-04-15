1fd3bf5cfea8c9e01294a06dc8335565e427b351
黃瓘閎 <david840801@gmail.com>
2023-04-15 17:20:39 +0800

    to android native

--- android/app/src/main/kotlin/com/guanhong/appworks_school_flutter_program/MainActivity.kt
+++ android/app/src/main/kotlin/com/guanhong/appworks_school_flutter_program/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.guanhong.appworks_school_flutter_program
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private lateinit var channel: MethodChannel
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "yourpackageName/channelName")
+        channel.setMethodCallHandler { _, _ ->
+            startActivity(Intent(this, TapPayActivity::class.java))
+        }
+    }
 }
---
+++ android/app/src/main/kotlin/com/guanhong/appworks_school_flutter_program/TapPayActivity.kt
@@ -0,0 +1,12 @@
+package com.guanhong.appworks_school_flutter_program
+
+import android.app.Activity
+import android.os.Bundle
+
+class TapPayActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tap_pay)
+    }
+}
