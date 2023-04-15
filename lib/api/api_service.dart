d054583a6fcab5b579ec59db8964972330e25ba5
黃瓘閎 <david840801@gmail.com>
2023-04-15 12:53:05 +0800

    use dio

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,14 +1,13 @@
 import 'package:appworks_school_flutter_program/api/product_list_response.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:dio/dio.dart';
 
 class ApiService {
 
   final baseUrl = 'https://api.appworks-school.tw/api/1.0/products/';
+  final dio = Dio();
 
   Future<ProductListResponse> getProductList(String productName) async {
-    var uri = Uri.parse('$baseUrl$productName');
-    var response = await http.get(uri);
-    return ProductListResponse.fromMap(jsonDecode(response.body));
+    final response = await dio.get('$baseUrl$productName');
+    return ProductListResponse.fromMap(response.data);
   }
 }
