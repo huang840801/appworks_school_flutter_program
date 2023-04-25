a603048b4315a970668a9bb758958bdf804468fa
david.huang <david.huang@ark88.net>
2023-04-25 15:29:24 +0800

    add maker

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'map_screen.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(25.0339206, 121.5636985);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[700],
-      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
-        ),
-        body: GoogleMap(
-          zoomGesturesEnabled: true,
-          onMapCreated: _onMapCreated,
-          mapType: MapType.normal,
-          compassEnabled: true,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0, bearing: 30, tilt: 50),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Maps Sample App'),
+          ),
+          body: const MapScreen()),
     );
   }
 }
---
+++ lib/map_screen.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final LatLng _initialLocation = const LatLng(25.048472, 121.532385);
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+  final CameraPosition _schoolPosition = const CameraPosition(target: LatLng(25.038472, 121.532385), zoom: 20);
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+  }
+
+  void addCustomIcon() {
+    BitmapDescriptor.fromAssetImage(const ImageConfiguration(), "assets/pin.png").then(
+      (icon) {
+        setState(() {
+          markerIcon = icon;
+        });
+      },
+    );
+  }
+
+  Future<void> _goToSchool() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_schoolPosition));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            initialCameraPosition: CameraPosition(
+              target: _initialLocation,
+              zoom: 10,
+            ),
+            circles: {
+              Circle(
+                circleId: const CircleId('Circle'),
+                center: const LatLng(25.048472, 121.532385),
+                radius: 4000,
+                fillColor: Colors.blue.shade100.withOpacity(0.5),
+                strokeColor: Colors.blue.shade100.withOpacity(0.5),
+              )
+            },
+            markers: {
+              Marker(
+                markerId: const MarkerId('Marker1'),
+                position: const LatLng(25.048472, 121.532385),
+                draggable: true,
+                onDragEnd: (value) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("latitude=${value.latitude}, longitude=${value.longitude}"),
+                  ));
+                },
+                icon: markerIcon,
+              ),
+              const Marker(
+                markerId: MarkerId('Marker2'),
+                position: LatLng(25.047472, 121.332385),
+              ),
+              const Marker(
+                markerId: MarkerId('Marker3'),
+                position: LatLng(25.047972, 121.432385),
+              ),
+              const Marker(
+                markerId: MarkerId('Marker4'),
+                position: LatLng(24.907472, 121.502385),
+              ),
+              const Marker(
+                markerId: MarkerId('Marker5'),
+                position: LatLng(25.056472, 121.722385),
+              ),
+              const Marker(
+                markerId: MarkerId('Marker6'),
+                position: LatLng(25.037472, 121.632385),
+              ),
+            },
+          ),
+          Positioned(
+              left: 10,
+              bottom: 40,
+              child: FloatingActionButton.extended(
+                onPressed: _goToSchool,
+                label: const Text('Go To School!'),
+                icon: const Icon(Icons.school),
+              ))
+        ],
+      ),
+    );
+  }
+}
